d24a0df886b5fcad9195abd74ae4047da2bc0d2e
yjjewg11 <41049630@qq.com>
2015-09-24 23:58:26 +0800

    修复互动加载更多问题. 今日签到增加详细显示. 统计结束日期设置为23:59:59

--- src/main/webapp/jsx/kd_react.js
+++ src/main/webapp/jsx/kd_react.js
@@ -645,10 +645,11 @@ var Classnews_reply_list = React.createClass({
 	load_more_data:function(){
 		$("#"+this.classnewsreply_list_div).append("<div id="+this.classnewsreply_list_div+this.pageNo+">加载中...</div>");
 		var re_data=this.state.replyPage;
-		if(!re_data){
-			re_data=commons_ajax_reply_list(this.props.uuid,this.classnewsreply_list_div+this.pageNo,this.pageNo,Classnews_reply_list_listshow);
-		}else{
+		if(re_data&& this.pageNo==1){
 			this.loadByFirst(this.classnewsreply_list_div+this.pageNo);
+		}else{
+			re_data=commons_ajax_reply_list(this.props.uuid,this.classnewsreply_list_div+this.pageNo,this.pageNo,Classnews_reply_list_listshow);
+			
 		}
 		if(!re_data)return;
 		if(re_data.data.length<re_data.pageSize){
@@ -5528,7 +5529,8 @@ render: function() {
            <tr className={className} >
            <td> <a href="javascript:void(0);"  onClick={ajax_class_students_look_info.bind(this,event.uuid)}  >{event.name}</a></td>
              <td>{event.cardID}</td>
-             <td className={event.qiandao?"":"px_color_red"}>{event.qiandao?"已签到":"无"}</td>
+			  <td className={event.qiandao?"":"px_color_red"}>{event.qiandao?"已签到":"无"}<AMUIReact.Button onClick={ajax_ClassCard_info_byRight.bind(this,event.uuid)} amStyle="success">详情</AMUIReact.Button></td>
+
              </tr> 
          );
        }
