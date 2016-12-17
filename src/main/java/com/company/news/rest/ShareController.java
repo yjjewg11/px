bed2d8454839c88976bb4bdc3502d3d0baf42a1a
yjjewg11 <41049630@qq.com>
2016-12-17 15:59:56 +0800

    增加接口，返回ios https要求地址，和分享缩略图地址。 官网支持https

--- src/main/java/com/company/news/rest/ShareController.java
+++ src/main/java/com/company/news/rest/ShareController.java
@@ -518,6 +518,8 @@ public class ShareController extends AbstractRESTController {
 	}
 	
 	private static String _KDWebUrl=null;
+	
+	
 	/**
 	 * 获取老师web登录地址.
 	 * @param model
@@ -543,6 +545,60 @@ public class ShareController extends AbstractRESTController {
 			}
 			
 			model.put("url",_KDWebUrl);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			responseMessage.setStatus(RestConstants.Return_ResponseMessage_failed);
+			responseMessage.setMessage("服务器异常:"+e.getMessage());
+			return "";
+		}
+		responseMessage.setStatus(RestConstants.Return_ResponseMessage_success);
+		return "";
+	}
+	
+	
+	private static String _KDWebUrlIOS=null;
+	
+	private static String shareImgUrlIOS=null;
+	/**
+	 * 获取老师web登录地址. ios https 
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getConfigOfIOS", method = RequestMethod.GET)
+	public String getConfigOfIOS(ModelMap model, HttpServletRequest request) {
+		ResponseMessage responseMessage = RestUtil
+				.addResponseMessageForModelMap(model);
+		try {
+			
+			if(_KDWebUrlIOS==null){
+					List list=this.nSimpleHibernateDao
+							.getHibernateTemplate().find(
+									"select description from BaseDataList where typeuuid='KDWebUrlIOS' and datakey=1");
+					
+						if(list!=null&&list.size()>0){
+							_KDWebUrl=list.get(0)+"";
+						}else{
+							_KDWebUrl="https://kd.wenjienet.com/px-rest/kd/index1.html?v1";
+						}
+			}
+			
+			
+			if(shareImgUrlIOS==null){
+				List list=this.nSimpleHibernateDao
+						.getHibernateTemplate().find(
+								"select description from BaseDataList where typeuuid='shareImgUrlIOS' and datakey=1");
+					
+					if(list!=null&&list.size()>0){
+						shareImgUrlIOS=list.get(0)+"";
+					}else{
+						shareImgUrlIOS="https://www.wenjienet.com/px-rest/i/share_logo.png";
+					}
+		}
+		
+			model.put("url",_KDWebUrl);
+			model.put("shareImgUrl",shareImgUrlIOS);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
